e99c2700358ff8e226b76c0bf76e90d4475f1303
Andrew Davisson <akdavisson4@gmail.com>
2019-06-12 23:34:43 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -21,8 +21,7 @@ class Artist
   
   def add_song_by_name(song_name)
     song = Song.new(song_name)
-    song.artist = self
-    @songs << song
+    add_song(song)
   end
   
   def self.song_count
